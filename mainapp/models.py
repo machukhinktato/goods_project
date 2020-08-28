a2c8ca9d0b2083aaade72df84d96b2de5448d819
machukhinktato <tarabrinmv@gmail.com>
2020-08-28 18:07:03 +0300

    models upgraded

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Goods_Items(models.Model):
+class GoodsItems(models.Model):
     name = models.CharField(verbose_name='товары', max_length=64, blank=True, unique=True)
     price = models.CharField(verbose_name='цена', max_length=64, blank=True)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import GoodsItems
 
 
 def index(request):
+    data = GoodsItems.objects.all()
     context = {
         'greetings': 'hello',
-        'phrase': 'mike was there'
+        'phrase': 'mike was there',
+        'data': data,
     }
     return render(request, 'index.html', context)
