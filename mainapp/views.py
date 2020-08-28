256986d21e277c419435e5a8f505499b0f6d476b
machukhinktato <tarabrinmv@gmail.com>
2020-08-28 19:14:48 +0300

    done

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import View
 from .models import GoodsItems
 
 
-def index(request):
-    data = GoodsItems.objects.all()
-    context = {
-        'greetings': 'hello',
-        'phrase': 'mike was there',
-        'data': data,
-    }
-    return render(request, 'index.html', context)
+class GoodsDetails():
+    model = GoodsItems
+    template = 'mainapp/index.html'
+
+# def index(request):
+#     data = GoodsItems.objects.all()
+#     context = {
+#         'greetings': 'hello',
+#         'phrase': 'mike was there',
+#         'data': data,
+#     }
+#     return render(request, 'index.html', context)
