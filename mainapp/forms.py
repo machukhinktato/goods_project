ee5fea09199452aaa63f4bcaa2fd69b4442a9df4
machukhinktato <tarabrinmv@gmail.com>
2020-08-30 16:23:52 +0300

    done

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -21,21 +21,26 @@ class GoodsForm(forms.ModelForm):
 
     class Meta:
         model = GoodsItems
-        fields = ['name', 'price']
+        fields = '__all__'
 
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'price': forms.TextInput(attrs={'class': 'form-control'}),
-        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+#         widgets = {
+#             'name': forms.TextInput(attrs={'class': 'form-control'}),
+#             'price': forms.TextInput(attrs={'class': 'form-control'}),
+#         }
+# #
 #
-
-    def clean_name(self):
-        new_name = self.cleaned_data['name'].lower()
-
-        # if new_name == 'create':
-        #     raise ValidationError('slug may not be "Create"')
-        if GoodsItems.objects.filter(name__iexact=new_name).count():
-            raise ValidationError(
-                f'Slug must be a unique. We have a slug "{new_name}" already'
-            )
-        return new_name
+#     def clean_name(self):
+#         new_name = self.cleaned_data['name'].lower()
+#
+#         # if new_name == 'create':
+#         #     raise ValidationError('slug may not be "Create"')
+#         if GoodsItems.objects.filter(name__iexact=new_name).count():
+#             raise ValidationError(
+#                 f'Slug must be a unique. We have a slug "{new_name}" already'
+#             )
+#         return new_name
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,27 +1,34 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
 from .models import GoodsItems
 from .forms import GoodsForm
 
 
-class GoodsDetails(View):
-    model_form = GoodsForm
-    template = 'mainapp/index.html'
+def goods_list(request):
+    goods = GoodsItems.objects.all()
+    return render(request, 'index.html', context={'goods':goods})
 
-    def get(self, request):
-        form = self.model_form
-        return render(request, self.template, context={'form': form})
 
-    def post(self, request):
-        bound_form = self.model_form(request.POST)
+# class GoodsDetails(View):
+    # model_form = GoodsItems
+#     template = 'mainapp/index.html'
 
-        if bound_form.is_valid():
-            new_obj = bound_form.save()
-            return redirect(new_obj)
-        return render(
-            request,
-            self.template,
-            context={
-                'form': bound_form
-            }
-        )
+
+    # return
+    # def get(self, request):
+    #     form = self.model_form
+    #     return render(request, self.template, context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = self.model_form(request.POST)
+    #
+    #     if bound_form.is_valid():
+    #         new_obj = bound_form.save()
+    #         return redirect(new_obj)
+    #     return render(
+    #         request,
+    #         self.template,
+    #         context={
+    #             'form': bound_form
+    #         }
+    #     )
