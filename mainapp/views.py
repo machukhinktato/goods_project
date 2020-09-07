5eb73db92c4441274e2a819df652ec7fb97c0bf6
machukhinktato <tarabrinmv@gmail.com>
2020-09-07 12:49:48 +0300

    done

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
+from django.template.loader import render_to_string
 from .models import GoodsItems
 from .forms import GoodsForm
 from django.http import HttpResponseRedirect
@@ -25,7 +26,7 @@ class GoodsList(View):
 class GoodsDetails(View):
     model_form = GoodsForm
     template = 'mainapp/goods_add.html'
-    dict = {}
+    data = {}
 
     def get(self, request):
         form = self.model_form
@@ -33,14 +34,32 @@ class GoodsDetails(View):
 
     def post(self, request):
         if request.method == "POST":
-            bound_form = self.model_form(request.POST)
-        if bound_form.is_valid():
-            bound_form.save()
-            return HttpResponseRedirect('/')
-        return render(
-            request,
-            self.template,
-            context={
-                'form': bound_form
-            }
-        )
+            form = GoodsForm(request.POST)
+            if form.is_valid():
+                form.save()
+                data['valid'] = True
+                goods = GoodsItems.objects.all()
+                data['goods_html'] = render_to_string(
+                    'index.html', {'goods': goods})
+            else:
+                data['form_html'] = render_to_string(
+                    'goods_add.html', {'form': form}, request=request)
+        else:
+            data['valid'] = False
+            data['form_html'] = render_to_string(
+                'index.html', {'form': model_form()}, request=request)
+
+        return JsonResponse(data)
+
+
+        #     bound_form = self.model_form(request.POST)
+        # if bound_form.is_valid():
+        #     bound_form.save()
+        #     return HttpResponseRedirect('/')
+        # return render(
+        #     request,
+        #     self.template,
+        #     context={
+        #         'form': bound_form
+        #     }
+        # )
