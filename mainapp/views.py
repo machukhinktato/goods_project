3d50643d9372f4c29b2672a98fc3f6fa901aacf8
machukhinktato <tarabrinmv@gmail.com>
2020-09-12 13:18:21 +0300

    done

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,83 +7,69 @@ from .forms import GoodsForm
 from django.http import HttpResponseRedirect
 
 
-# class GoodsList:
-#     model = GoodsItems
-#     template = 'index.html'
-#
-#     def get(self, request):
-#         goods = self.model.objects.all()
-#         return render(
-#             request,
-#             self.template,
-#             context={
-#                 self.model.__name__.lower(): goods,
-#                 'goods': goods,
-#                 'detail': True,
-#             }
-#         )
-
-
-class GoodsDetails(View):
-    model_form = GoodsForm
-    template = 'mainapp/goods_add.html'
-    data = {}
+class GoodsList:
+    model = GoodsItems
+    template = 'index.html'
 
     def get(self, request):
-        form = GoodsForm()
-        return render(request, self.template, context={'form': form})
-
-    @staticmethod
-    def post(request):
-        if request.method == "POST":
-            form = model_form(request.POST)
-            if form.is_valid():
-                form.save()
-                data['form_is_valid'] = True
-                goods = model.objects.all()
-                data['goods_html'] = render_to_string(
-                    'mainapp/list.html', {'goods': goods})
-            else:
-                data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': form}, request=request)
-        else:
-            data['form_is_valid'] = False
-            data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': model_form()}, request=request)
-
-        return JsonResponse(data)
+        goods = self.model.objects.all()
+        return render(
+            request,
+            self.template,
+            context={
+                self.model.__name__.lower(): goods,
+                'goods': goods,
+                'detail': True,
+            }
+        )
 
 
-                                        #     bound_form = self.model_form(request.POST)
-                                        # if bound_form.is_valid():
-                                        #     bound_form.save()
-                                        #     return HttpResponseRedirect('/')
-                                        # return render(
-                                        #     request,
-                                        #     self.template,
-                                        #     context={
-                                        #         'form': bound_form
-                                        #     }
-                                        # )
-
-def GoodsList(request):
-    return render(request, 'mainapp/index.html', {'goods': GoodsItems.objects.all()})
-
-
-# def GoodsDetails(request):
-#     data = dict()
-#     if request.method == 'POST':
-#         form = GoodsForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             data['correct'] = True
-#             goods = GoodsItems.objects.all()
-#             data['goods_html'] = render_to_string('mainapp/list.html', {'goods': goods})
+# class GoodsDetails(View):
+#     model_form = GoodsForm
+#     template = 'mainapp/goods_add.html'
+#     data = {}
+#
+#     def get(self, request):
+#         form = GoodsForm()
+#         return render(request, self.template, context={'form': form})
+#
+#     @staticmethod
+#     def post(request):
+#         if request.method == 'POST':
+#             form = GoodsForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 data['correct'] = True
+#                 goods = GoodsItems.objects.all()
+#                 data['goods_html'] = render_to_string('mainapp/list.html', {'goods': goods})
+#             else:
+#                 data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': form}, request=request)
+#
 #         else:
-#             data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': form}, request=request)
+#             data['correct'] = False
+#             data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': GoodsForm()}, request=request)
 #
-#     else:
-#         data['correct'] = False
-#         data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': GoodsForm()}, request=request)
+#         return JsonResponse(data)
 #
-#     return JsonResponse(data)
 #
-# or not
+# def GoodsList(request):
+#     return render(request, 'mainapp/index.html', {'goods': GoodsItems.objects.all()})
+
+
+def GoodsDetails(request):
+    data = dict()
+    if request.method == 'POST':
+        form = GoodsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data['correct'] = True
+            goods = GoodsItems.objects.all()
+            data['goods_html'] = render_to_string('mainapp/list.html', {'goods': goods})
+        else:
+            data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': form}, request=request)
+
+    else:
+        data['correct'] = False
+        data['form_html'] = render_to_string('mainapp/goods_add.html', {'form': GoodsForm()}, request=request)
+
+    return JsonResponse(data)
